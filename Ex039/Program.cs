370286f7b0b0d9545eac2945378505b65f375557
Elena <velenavl@inbox.ru>
2022-09-15 00:33:13 +0300

    Ex 039

---
+++ Ex039/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая перевернет одномерный массив (последний элемент будет на первом месте, а первый - на последнем)
+int[] array = new int[] {1,2,3,4,5};
+PrintArray(array);
+int [] reverseArray = ReverseArray(array);
+Console.WriteLine();
+PrintArray(reverseArray);
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+      Console.Write($"{array[i]} ");  
+    }
+}
+
+int[] ReverseArray(int[] array)
+{
+int[] reverseArray = new int[array.Length];
+
+for (int i = 0; i < array.Length; i++)
+{
+   reverseArray[i] = array[array.Length - 1 - i];
+}
+return reverseArray;
+}
